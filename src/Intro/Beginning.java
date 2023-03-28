0c010f3dc326870bc811750250d70f437274a6af
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2023-03-28 12:58:44 +0300

    Learn: print, println, comment line, multiple comment line and info comment line

---
+++ src/Intro/Beginning.java
@@ -0,0 +1,19 @@
+package Intro;
+
+public class Beginning {
+    public static void main(String[] args) {
+
+        /**
+         * @author Dogac Tanriverdi, 2023 - Kodluyoruz
+         */
+
+        // Text lines
+        System.out.println("Hello Java!");
+        System.out.print("Have fun :)");
+
+        /*
+           This is a multiple comment line
+           This is a comment on multiple comment line
+         */
+    }
+}
