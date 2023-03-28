5848621871d5f28d7c059b2296d44e18ce64df1a
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2023-03-28 14:37:32 +0300

    Learn: Strings

---
+++ src/datatypes/Strings.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        System.out.println("---------------------- String ----------------------");
+
+        String vStr = "Java 101 ";
+        String vStr2 = "Lesson.";
+        System.out.println(vStr + vStr2);
+
+        System.out.println("------");
+
+        String str1 = "Hello";
+        String str2 = "Java";
+        char c1 = '!';
+        System.out.println(str1 + " " + str2 + c1);
+    }
+}
