d6b3d1b74754616997067d000ed0123591bb57ac
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2023-03-30 18:42:49 +0300

    Learn: Switch Case

---
+++ src/conditionalexpressions/SwitchCase.java
@@ -0,0 +1,49 @@
+package conditionalexpressions;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        int day = 7;
+
+        // Usage 1
+        switch (day) {
+            case 1:
+                System.out.println("Today is Monday");
+                break;
+            case 2:
+                System.out.println("Today is Tuesday");
+                break;
+            case 3:
+                System.out.println("Today is Wednesday");
+                break;
+            case 4:
+                System.out.println("Today is Thursday");
+                break;
+            case 5:
+                System.out.println("Today is Friday");
+                break;
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("Please enter a valid number!");
+        }
+
+        System.out.println("---------------------------");
+
+        // Usage 2
+        switch (day) {
+            case 1 -> System.out.println("Today is Monday");
+            case 2 -> System.out.println("Today is Tuesday");
+            case 3 -> System.out.println("Today is Wednesday");
+            case 4 -> System.out.println("Today is Thursday");
+            case 5 -> System.out.println("Today is Friday");
+            case 6 -> System.out.println("Today is Saturday");
+            case 7 -> System.out.println("Today is Sunday");
+            default -> System.out.println("Please enter a valid number!");
+        }
+    }
+}
