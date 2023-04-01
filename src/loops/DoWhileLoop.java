f81a6bfac43d52db12123dc361efea54bbf83505
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2023-04-01 11:53:38 +0300

    Learn: While Loop

---
+++ src/loops/DoWhileLoop.java
@@ -0,0 +1,33 @@
+package loops;
+
+import java.util.Scanner;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int password;
+        boolean askPassword = true;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter your password: ");
+            password = input.nextInt();
+            if (password == 123) {
+                System.out.println("Correct!");
+                askPassword = false;
+            } else {
+                System.out.println("Incorrect! Try again");
+            }
+        } while (askPassword);
+
+        int i = 1, j = 1;
+        while (i < 3) {
+            do {
+                System.out.print(j + ",");
+                j++;
+            } while (j < 4);
+            i++;
+        }
+    }
+}
