5c4673fc6e992654073cca6983ae2db8aa8bf73f
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2023-04-03 11:29:55 +0300

    Learn: Continue and Break Command

---
+++ src/loops/ContinueAndBreakCommand.java
@@ -0,0 +1,37 @@
+package loops;
+
+public class ContinueAndBreakCommand {
+    public static void main(String[] args) {
+
+        System.out.println("--- For Loop - Break Command ---");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 5) {
+                System.out.println("Found 5");
+                break;
+            }
+        }
+
+        System.out.println("\n--- For Loop - Continue Command ---");
+
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("\n--- While Loop - Continue Command ---");
+
+        int i = 0;
+        while (i <= 10) {
+            i++;
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        // Same in Do-While
+    }
+}
